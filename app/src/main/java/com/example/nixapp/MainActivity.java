c6c3c4c9501486778fcf7d5cb80c4e85bc477777
EdalfnerUnityBrited <60933236+EdalfnerUnityBrited@users.noreply.github.com>
2020-03-09 16:36:40 -0600

    Hoy

--- app/src/main/java/com/example/nixapp/MainActivity.java
+++ app/src/main/java/com/example/nixapp/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.nixapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Conexion conne= new Conexion();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        conne.connectionBD();
+        conexionExitosa();
+    }
+
+    private void conexionExitosa() {
+        if (conne.conexion!=null){
+            Toast.makeText(getApplicationContext(),"La conexion se ha establecido",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"Conexion erronea",Toast.LENGTH_SHORT).show();
+        }
     }
 }
