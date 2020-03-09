5bb17ae8b8d85558a274aa8ff2fd22e6fee8ca33
elderdeveloper15 <54344130+elderdeveloper15@users.noreply.github.com>
2020-03-09 16:35:36 -0600

    CAMBIO EN PAQUETES

--- app/src/main/java/com/example/nixapp/UI/MainActivity.java
+++ app/src/main/java/com/example/nixapp/UI/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.nixapp;
+package com.example.nixapp.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.nixapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
