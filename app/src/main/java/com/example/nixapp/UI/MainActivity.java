72ca5b0b08cc7adee98404ae2897729649302157
elderdeveloper15 <54344130+elderdeveloper15@users.noreply.github.com>
2020-03-09 21:18:27 -0600

    MAIN CON CAMBIO A INICIO DE SESION

--- app/src/main/java/com/example/nixapp/UI/MainActivity.java
+++ app/src/main/java/com/example/nixapp/UI/MainActivity.java
@@ -2,20 +2,31 @@ package com.example.nixapp.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.nixapp.DB.Conexion;
 import com.example.nixapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Conexion conne= new Conexion();
+    
+    Button btnLogin,btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = (Button) findViewById(R.id.buttonLogin);
+        btnSignup = (Button) findViewById(R.id.buttonSignup);
+        btnLogin.setOnClickListener(this);
+        btnSignup.setOnClickListener(this);
+
         conne.connectionBD();
         conexionExitosa();
     }
@@ -26,6 +37,22 @@ public class MainActivity extends AppCompatActivity {
         }
         else{
             Toast.makeText(getApplicationContext(),"Conexion erronea",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        int i = view.getId();
+        Intent q;
+        switch (i){
+            case R.id.buttonLogin:{
+                q = new Intent(view.getContext(),InicioSesion.class);
+                startActivity(q);
+                break;
+            }
+            case R.id.buttonSignup:{
+                break;
+            }
         }
     }
 }
