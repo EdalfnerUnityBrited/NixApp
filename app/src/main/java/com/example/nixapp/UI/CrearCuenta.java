15583dc3653fd5c3136beaf9d6923eb9d34c054b
az0511258@gmail.com <abraham1234>
2020-03-11 21:32:52 -0600

    Se hizo la activity de Crear cuenta

---
+++ app/src/main/java/com/example/nixapp/UI/CrearCuenta.java
@@ -0,0 +1,14 @@
+package com.example.nixapp.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.nixapp.R;
+public class CrearCuenta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_cuenta);
+    }
+}
