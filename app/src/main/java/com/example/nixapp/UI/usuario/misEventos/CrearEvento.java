9effc72a6184a04bd327a80b4718584156922f1d
az0511258@gmail.com <abraham1234>
2020-04-02 21:19:51 -0600

    Se agrego el mockup de crear evento

--- app/src/main/java/com/example/nixapp/UI/usuario/misEventos/CrearEvento.java
+++ app/src/main/java/com/example/nixapp/UI/usuario/misEventos/CrearEvento.java
@@ -1,7 +1,21 @@
 package com.example.nixapp.UI.usuario.misEventos;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +27,26 @@ import com.example.nixapp.conn.NixService;
 import com.example.nixapp.conn.results.EventosResult;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.regex.Pattern;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class CrearEvento extends AppCompatActivity {
+
+    DatePickerDialog picker;
+    EditText eText;
+    private ArrayList<EventosItems> mEventsList;
+    private EventosAdapter mAdapter;
+    TimePickerDialog picker2;
+    EditText eText2;
+
+    Switch simpleSwitch1;
+    CheckBox cover;
 
     NixService nixService;
     NixClient nixClient;
@@ -35,14 +63,14 @@ public class CrearEvento extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_evento);
         retrofitinit();
-        nombre_evento="peda";
-        privacidad=0;
+        nombre_evento="Fiesta";
+        privacidad=1;
         categoria_evento=3;
-        fecha="2020-04-12";
-        hora="12:00";
-        lugar="la santa zapopan jalisco mexico";
-        descripcion="hasta que el cuerpo aguante";
-        cupo=300;
+        fecha="2020-04-20";
+        hora="17:00";
+        lugar="Francisco Rojas Gonzalez #577";
+        descripcion="Habra patos, muchos patos";
+        cupo=100;
         final Eventos requestSample = new Eventos(nombre_evento, privacidad, categoria_evento, fecha, hora, lugar, descripcion, cupo);
         Call<EventosResult> call = nixService.eventos(requestSample);
         call.enqueue(new Callback<EventosResult>() {
@@ -67,7 +95,150 @@ public class CrearEvento extends AppCompatActivity {
                 Toast.makeText(CrearEvento.this, "Error en la conexion", Toast.LENGTH_SHORT).show();
             }
         });
+        //////////////////////
+        eText=(EditText) findViewById(R.id.fecha);
+        eText.setInputType(InputType.TYPE_NULL);
+        eText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Locale locale = getResources().getConfiguration().locale;
+                Locale.setDefault(locale);
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(CrearEvento.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                eText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
 
+        eText2=(EditText) findViewById(R.id.hora_evento);
+        eText2.setInputType(InputType.TYPE_NULL);
+        eText2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Locale locale2 = getResources().getConfiguration().locale;
+                Locale.setDefault(locale2);
+                final Calendar cldr2 = Calendar.getInstance();
+                int hour = cldr2.get(Calendar.HOUR_OF_DAY);
+                int minutes = cldr2.get(Calendar.MINUTE);
+                // time picker dialog
+                picker2 = new TimePickerDialog(CrearEvento.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker tp, int sHour, int sMinute) {
+                                eText2.setText(sHour + ":" + sMinute);
+                            }
+                        }, hour, minutes, true);
+                picker2.show();
+            }
+        });
+
+        initList();
+
+        Spinner spinners = findViewById(R.id.spinnerSimple);
+
+        mAdapter = new EventosAdapter(this, mEventsList);
+        spinners.setAdapter(mAdapter);
+
+        spinners.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                EventosItems clickedItem = (EventosItems) parent.getItemAtPosition(position);
+                String clickedName = clickedItem.getEventoName();
+                Toast.makeText(CrearEvento.this, clickedName + " selecteccionada", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        simpleSwitch1 = (Switch) findViewById(R.id.servicios);
+        final Button enables = findViewById(R.id.buttonIrCatalogo);
+        final Button info = findViewById(R.id.info);
+
+        simpleSwitch1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String statusSwitch1, statusSwitch2;
+                if (simpleSwitch1.isChecked()) {
+                    info.setVisibility(View.VISIBLE);
+                    enables.setVisibility(View.VISIBLE);
+                    Toast.makeText(CrearEvento.this, "Ve al catalogo para ver los servicios", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    info.setVisibility(View.GONE);
+                    enables.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        cover = (CheckBox) findViewById(R.id.cover);
+        final EditText cover_val = findViewById(R.id.cover_valor);
+
+        cover.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String statusSwitch1, statusSwitch2;
+                if (cover.isChecked()) {
+                    cover_val.setVisibility(View.VISIBLE);
+                    Toast.makeText(CrearEvento.this, "Agrega el cover", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    cover_val.setText("");
+                    cover_val.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        final TextView mostrarCorreos = findViewById(R.id.mostrarCorreos);
+        final EditText correos = findViewById(R.id.correos);
+        Button insertar = findViewById(R.id.correoAgregado);
+
+        insertar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (correos.getText().toString().isEmpty()) {
+                    Toast.makeText(CrearEvento.this, "Agrega algun correo porfavor", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    if(validarEmail(correos.getText().toString()))
+                    {
+                        mostrarCorreos.setText(mostrarCorreos.getText()+"\n" + correos.getText());
+                        correos.setText("");
+                    }
+                    else
+                    {
+                        Toast.makeText(CrearEvento.this, "Eso no es un correo -.-'", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        /////////////////////
+    }
+
+    private boolean validarEmail(String email) {
+        Pattern pattern = Patterns.EMAIL_ADDRESS;
+        return pattern.matcher(email).matches();
+    }
+
+    private void initList() {
+        mEventsList = new ArrayList<>();
+        mEventsList.add(new EventosItems("Elige una categoria", R.drawable.select_some));
+        mEventsList.add(new EventosItems("Compromisos", R.drawable.compromisos));
+        mEventsList.add(new EventosItems("Mega Eventos", R.drawable.mega));
+        mEventsList.add(new EventosItems("Galas", R.drawable.galas));
+        mEventsList.add(new EventosItems("Empresariales", R.drawable.empresariales));
+        mEventsList.add(new EventosItems("Festejos", R.drawable.festejos));
+        mEventsList.add(new EventosItems("Religiosos", R.drawable.religiosos));
     }
 
     private void retrofitinit() {
