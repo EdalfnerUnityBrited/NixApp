b05dca11a3db485bdca04b5d97e4e8f051a6bca7
elderdeveloper15 <54344130+elderdeveloper15@users.noreply.github.com>
2020-05-14 18:18:58 -0500

    Agregado el floating button a proveedor

--- app/src/main/java/com/example/nixapp/UI/proveedor/misServicios/CrearServiciosFragmentProveedor.java
+++ app/src/main/java/com/example/nixapp/UI/proveedor/misServicios/CrearServiciosFragmentProveedor.java
@@ -10,11 +10,21 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.nixapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class CrearServiciosFragmentProveedor extends Fragment {
+public class CrearServiciosFragmentProveedor extends Fragment implements View.OnClickListener {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_crear_servicios_proveedor,container,false);
+        View view = inflater.inflate(R.layout.fragment_crear_servicios_proveedor,container,false);
+        FloatingActionButton actionButton = (FloatingActionButton) view.findViewById(R.id.nuevo_servicio);
+        actionButton.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
