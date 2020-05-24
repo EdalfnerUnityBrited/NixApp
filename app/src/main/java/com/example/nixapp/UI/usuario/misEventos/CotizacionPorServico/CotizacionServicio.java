2a65c39a6be3c13e14d5c271f3d1887663e4bcdf
az0511258@gmail.com <abraham1234>
2020-05-24 15:09:04 -0500

    Cotizaciones vistas

---
+++ app/src/main/java/com/example/nixapp/UI/usuario/misEventos/CotizacionPorServico/CotizacionServicio.java
@@ -0,0 +1,16 @@
+package com.example.nixapp.UI.usuario.misEventos.CotizacionPorServico;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.nixapp.R;
+
+public class CotizacionServicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cotizacion_servicio);
+    }
+}
